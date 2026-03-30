6ddd2c79a9cb233cd836eb639746c03b15093385
Becky Spanner <129760627+bspan07@users.noreply.github.com>
2026-03-30 15:13:59 -0500

    Create 02_lowcoverage.sh

---
+++ 02_lowcoverage.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+#SBATCH --job-name=lowcov
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=20G
+#SBATCH --time=12:00:00
+
+module load minimap2/2.30
+module load bbmap/39.01
+
+OUTDIR="/output/path"
+ISOLATES="$1"          # isolates.txt
+LOWCOV=5
+
+cd "$OUTDIR"
+
+mapfile -t SAMPLES < <(cut -f1 "$ISOLATES")
+SAMPLE="${SAMPLES[$SLURM_ARRAY_TASK_ID]}"
+
+cat "${SAMPLE}.asm.hic.hap1.p_ctg.fa" "${SAMPLE}.asm.hic.hap2.p_ctg.fa" > "${SAMPLE}_combined.fasta"
+
+minimap2 -ax map-hifi "${SAMPLE}_combined.fasta" "${SAMPLE}_reads.fasta" > "${SAMPLE}.sam"
+
+pileup.sh in="${SAMPLE}.sam" out="${SAMPLE}_pileup.txt"
+
+awk -v x="$LOWCOV" '$2 <= x {print $1}' "${SAMPLE}_pileup.txt" > "${SAMPLE}_low_cov.txt"
+
+awk 'FNR==NR {bad[$1]; next} /^>/ {keep=!(substr($1,2) in bad)} keep' \
+    "${SAMPLE}_low_cov.txt" "${SAMPLE}_combined.fasta" > "${SAMPLE}_lowcov_removed.fa"
